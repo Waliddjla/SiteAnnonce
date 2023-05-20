14cd4dd0d7e9eb7affb879e07ca784b32ffe9a07
Walid Djellali <walidbibo1997@gmail.com>
2023-05-20 20:11:31 +0100

    FAQ question finished

--- scripts/app.js
+++ scripts/app.js
@@ -16,3 +16,25 @@ btnNav.addEventListener('click', ()=> {
         imgBtn.style.heigth = "40px";
     }
 })
+
+// -----------------FAQ---------//
+const allCros = document.querySelectorAll ('.panneau-visible img');
+
+allCros.forEach(logo => {
+    logo.addEventListener('click', function() {
+        const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
+        const currentChoice = this.parentNode.parentNode.childNodes[3];
+        if(this.src.includes('plus')){
+            this.src ="/ressources/minus.svg";
+            currentChoice.style.height = height + 20 + "px";
+            currentChoice.style.opacity = 1;
+            currentChoice.style.padding = "20px 15px";
+        }else {
+            this.src ="/ressources/plus.svg";
+            currentChoice.style.height = 0;
+            currentChoice.style.opacity = 0;
+            currentChoice.style.padding = "0px 15px";
+        }
+       
+    })
+})
